622a690085f00ddb8f40540caba6492f7f80aa67
xzy <xzy.vip@qq.com>
2018-09-17 11:46:24 +0800

    管理后台-商品列表

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -24,8 +24,8 @@ class ProductsController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('商品列表');
+            //$content->description('description');
 
 
 
@@ -79,11 +79,38 @@ class ProductsController extends Controller
             $grid->id('ID')->sortable();
 
             //第二列显示title字段，由于title字段名和Grid对象的title方法冲突，所以用Grid的column()方法代替
-            $grid->column('title');
+            $grid->column('商品名称');
 
+            //是否上架
+            $grid->on_sale('是否上架')->dispaly(function($value){
+                return $value ? '是' : '否';
+            });
 
-            $grid->created_at();
-            $grid->updated_at();
+            //价格
+            $grid->price('价格');
+
+            //评分
+            $grid->rating('评分');
+
+            //销量
+            $grid->sold_count('销量');
+
+            //评论数
+            $grid->review_count('评论数');
+
+            $grid->created_at('添加时间');
+
+            $grid->actions(function ($actions) {
+                //$actions->disableView();
+                $actions->disableDelete();
+            });
+            $grid->tools(function ($tools) {
+                // 禁用批量删除按钮
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
+            });
+            //$grid->updated_at();
         });
     }
 
